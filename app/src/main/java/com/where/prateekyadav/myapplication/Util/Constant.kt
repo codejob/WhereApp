0b94be601483ce567f8c6ba8097665f2983dc56d
prateek.yadav <prateekyaad@gmail.com>
2018-01-18 17:46:15 +0530

    Merge branch 'master' of https://github.com/codejob/WhereApp
    # Conflicts:
    #	app/src/main/java/com/where/prateekyadav/myapplication/AlarmReceiverLocation.kt

--- app/src/main/java/com/where/prateekyadav/myapplication/Util/Constant.kt
+++ app/src/main/java/com/where/prateekyadav/myapplication/Util/Constant.kt
@@ -15,7 +15,7 @@ open class Constant {
         val E_WORKBOOK_DEBUG_TAG = "eWorkBook-"
         // Seconds
         val LOCATION_SYNC_INSTERVAL: Long =  10* 60 * 1000;
-        val LOCATION_SYNC_TIMEOUT: Long = 20 *1000;
+        val LOCATION_SYNC_TIMEOUT: Long = 40 *1000;
         val RECEIVER_ACTION: String = "LOCATION";
         var MIN_DISTANCE_RANGE: Int = 200;
         val alarmID: Int = 1001;
